7db18e28b25fa6ff4659787ec0f3e727110cd03e
jemguan <guan980234@gmail.com>
2025-02-03 00:31:18 -0500

    部署railway-8

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,21 +1,24 @@
 // Uncomment this file to enable instrumentation and observability using OpenTelemetry
 // Refer to the docs for installation instructions: https://docs.medusajs.com/learn/debugging-and-testing/instrumentation
 
-import { registerOtel } from "@medusajs/medusa"
-import { ZipkinExporter } from "@opentelemetry/exporter-zipkin"
+// import { registerOtel } from "@medusajs/medusa"
+// // If using an exporter other than Zipkin, require it here.
+// import { ZipkinExporter } from "@opentelemetry/exporter-zipkin"
 
-const exporter = new ZipkinExporter({
-  serviceName: 'medusa-starter-default',
-})
+// // If using an exporter other than Zipkin, initialize it here.
+// const exporter = new ZipkinExporter({
+//   serviceName: 'my-medusa-project',
+// })
 
-export function register() {
-  registerOtel({
-    serviceName: 'medusajs',
-    exporter,
-    instrument: {
-      http: true,
-      workflows: true,
-      query: true
-    },
-  })
-}
+// export function register() {
+//   registerOtel({
+//     serviceName: 'medusajs',
+//     // pass exporter
+//     exporter,
+//     instrument: {
+//       http: true,
+//       workflows: true,
+//       query: true
+//     },
+//   })
+// }
