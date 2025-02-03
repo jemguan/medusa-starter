850f772a784e162b4c2d9ea5e1c5014b8da7d949
jemguan <guan980234@gmail.com>
2025-02-02 23:40:59 -0500

    清理medusa admin

--- .medusa/server/public/admin/assets/chunk-UF3RZ3OU-BBnofnSP.js
+++
@@ -1,1 +0,0 @@
-import{al as a}from"./index-DBWnS55e.js";var e=a("product","*categories,-variants");export{e as P};
--- medusa-config.ts
+++ medusa-config.ts
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   },
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    backendUrl: process.env.MEDUSA_BACKEND_URL || "https://medusa-starter-production.up.railway.app",
   },
   modules: [
     {
